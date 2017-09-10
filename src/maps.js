166896ce3b9e3a26cb15a4c3fdc4e02f085536cf
Raymond Zhou <razh@users.noreply.github.com>
2017-09-10 15:59:52 -0400

    Add camera collision physics

--- src/maps.js
+++ src/maps.js
@@ -10,6 +10,14 @@ import {
   object3d_add,
   object3d_translateOnAxis,
 } from './object3d';
+import {
+  physics_create,
+  physics_add,
+  physics_bodies,
+  physics_update,
+  BODY_STATIC,
+  BODY_DYNAMIC,
+} from'./physics';
 import { vec3_create, vec3_normalize, vec3_set } from './vec3';
 
 export var createBasicMap = (scene, camera) => {
@@ -19,6 +27,8 @@ export var createBasicMap = (scene, camera) => {
 
   vec3_set(camera.position, 64, 64, 64);
   camera_lookAt(camera, vec3_create());
+  var cameraPhysics = physics_create(mesh_create(boxGeom_create(2, 2, 2)), BODY_DYNAMIC);
+  entity_add(camera, cameraPhysics);
 
   var cameraObject = object3d_create();
   object3d_add(cameraObject, camera);
@@ -27,36 +37,44 @@ export var createBasicMap = (scene, camera) => {
   var keys = keys_create();
   var cameraDirection = vec3_create();
 
+  var updateCamera = dt => {
+    var speed = 16;
+
+    var x = 0;
+    var z = 0;
+
+    if (keys.KeyW || keys.ArrowUp) { z--; }
+    if (keys.KeyS || keys.ArrowDown) { z++; }
+    if (keys.KeyA || keys.ArrowLeft) { x--; }
+    if (keys.KeyD || keys.ArrowRight) { x++; }
+
+    if (!x && !z) {
+      return;
+    }
+
+    if (keys.ShiftLeft || keys.ShiftRight) {
+      speed *= 4;
+    }
+
+    vec3_normalize(vec3_set(cameraDirection, x, 0, z));
+    object3d_translateOnAxis(camera, cameraDirection, speed * dt);
+  };
+
   entity_add(map, component_create({
     update(dt) {
-      var speed = 16;
-
-      var x = 0;
-      var z = 0;
-
-      if (keys.KeyW || keys.ArrowUp) { z--; }
-      if (keys.KeyS || keys.ArrowDown) { z++; }
-      if (keys.KeyA || keys.ArrowLeft) { x--; }
-      if (keys.KeyD || keys.ArrowRight) { x++; }
-
-      if (!x && !z) {
-        return;
-      }
-
-      if (keys.ShiftLeft || keys.ShiftRight) {
-        speed *= 4;
-      }
-
-      vec3_normalize(vec3_set(cameraDirection, x, 0, z));
-      object3d_translateOnAxis(camera, cameraDirection, speed * dt);
+      updateCamera(dt);
+      physics_update(physics_bodies(map));
     },
   }));
 
   object3d_add(
     map,
-    mesh_create(
-      boxGeom_create(8, 8, 8),
-      material_create(),
+    physics_add(
+      mesh_create(
+        boxGeom_create(8, 8, 8),
+        material_create(),
+      ),
+      BODY_STATIC,
     ),
   );
 
